50adae5884cd26f14ef322c8792d3ed3777ef2bd
Prabhaker Singh <iprabhakersingh@gmail.com>
2026-02-06 01:59:58 +0530

    Some changes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3000;
 await connectDB()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 app.get('/', (req, res)=> res.send("Server is live..."))
 app.use('/api/users', userRouter)
